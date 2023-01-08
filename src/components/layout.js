f6c40d029606a8f3ff69340c899ccce470adbb41
Rahul singh <rahulsingh19390@gmail.com>
2023-01-08 11:12:41 +0530

    Modular approach applied

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { LazyLoadImage } from "react-lazy-load-image-component";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import img1 from '../assets/img1.jpg';
@@ -7,7 +6,7 @@ import img2 from '../assets/img2.jpg';
 import img3 from '../assets/img3.jpg';
 import img4 from '../assets/img4.jpg';
 import img5 from '../assets/img5.jpg';
-import './styles.css';
+import styles from './styles.module.css';
 // import Image from './image.js';
 import { useEffect } from 'react';
 
@@ -28,28 +27,28 @@ function Layout() {
   return (
     <div>
       <main>
-        <div className="grid">
-            <div data-aos="zoom-in" data-aos-delay="200" className="gridItem">
+        <div className={styles.grid}>
+            <div data-aos="zoom-in" data-aos-delay="200" className={styles.gridItem}>
                 <img src={img1} alt="img" 
                 onMouseEnter={MouseEnter}
                 onMouseLeave={MouseLeave} />
             </div>
-            <div data-aos="zoom-in" data-aos-delay="200" className="gridItem">
+            <div data-aos="zoom-in" data-aos-delay="200" className={styles.gridItem}>
                 <img src={img2} alt="img" 
                 onMouseEnter={MouseEnter}
                 onMouseLeave={MouseLeave} />
             </div>
-            <div data-aos="zoom-in" data-aos-delay="200" className="gridItem">
+            <div data-aos="zoom-in" data-aos-delay="200" className={styles.gridItem}>
                 <img src={img3} alt="img" 
                 onMouseEnter={MouseEnter}
                 onMouseLeave={MouseLeave} />
             </div>
-            <div data-aos="zoom-in" data-aos-delay="200" className="gridItem">
+            <div data-aos="zoom-in" data-aos-delay="200" className={styles.gridItem}>
                 <img src={img4} alt="img" 
                 onMouseEnter={MouseEnter}
                 onMouseLeave={MouseLeave} />
             </div>
-            <div data-aos="zoom-in" data-aos-delay="200" className="gridItem">
+            <div data-aos="zoom-in" data-aos-delay="200" className={styles.gridItem}>
                 <img src={img5} alt="img" 
                 onMouseEnter={MouseEnter}
                 onMouseLeave={MouseLeave} />
